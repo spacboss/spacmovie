9b47325f3bf19d2c3dc8981ffe558316a6d6aa9c
Daniel Verejan <daniel.verejan@ellation.com>
2019-08-14 15:24:53 +0300

    Create basic components

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/about">SMH</Link>
+    </header>
+  );
+}
+
+export default Header;
